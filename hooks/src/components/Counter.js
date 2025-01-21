ead9dc8ad7c7abecf522403bd994f064f44f8563
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-21 16:38:50 -0500

    Update Counter.js

--- hooks/src/components/Counter.js
+++ hooks/src/components/Counter.js
@@ -1,7 +1,18 @@
+import { useState } from "react";
+
 function Counter(){
+    let[count,setCount]=useState();
+
+   
+
+    
     return(
         <>
         <h3>useState Hook Example</h3>
+        <div>
+            <p>you clicked {count} times</p>
+            <button onClick={funClick}>Click</button>
+        </div>
         </>
     );
 }
