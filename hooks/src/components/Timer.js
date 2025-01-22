ffeba23128a222d9daf6f63663fcfd732eccf97a
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-21 22:34:34 -0500

    Update Timer.js

--- hooks/src/components/Timer.js
+++ hooks/src/components/Timer.js
@@ -1,6 +1,16 @@
+import React, { useState, useEffect } from 'react';
 function Timer(){
+
+   let[count , setCount]=useState(0);
+
+   useEffect(() => {
+    document.title = `You clicked ${count} times`;
+  }, [count]);
+  
     return(
         <>
+        
+
         </>
     );
 }
