7bcab074d75510436eac058f4b85a8074fb6d2b8
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-22 19:35:46 -0500

    Update

--- hooks/src/components/ExpensiveCalculation.js
+++ hooks/src/components/ExpensiveCalculation.js
@@ -1,4 +1,21 @@
+import { useMemo, useState } from "react";
+
 function ExpensiveCalculation(num){
     console.log('Calculating...');
   return num * 2;
 }
+
+// ExpensiveCalculation Component using useMemo
+function ExpensiveCalculationComponent() {
+    const [count, setCount] = useState(0);
+  
+    const doubled = useMemo(() => ExpensiveCalculation(count), [count]);
+  
+    return (
+      <div>
+        <p>Count: {count}</p>
+        <p>Doubled: {doubled}</p>
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+      </div>
+    );
+  }
