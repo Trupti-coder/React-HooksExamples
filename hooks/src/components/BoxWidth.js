7810a44524e5b3f47b5ec23b8220dc5196fcc732
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-22 19:52:10 -0500

    Update BoxWidth.js

--- hooks/src/components/BoxWidth.js
+++ hooks/src/components/BoxWidth.js
@@ -1,6 +1,16 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
 function BoxWidth(){
+
+    const boxRef = useRef();
+  const [boxWidth, setBoxWidth] = useState(0);
+
+  useLayoutEffect(() => {
+    setBoxWidth(boxRef.current.offsetWidth);
+  }, []);
     return(
         <>
+        <h3>useLayoutEffect Hook Example</h3>
          <div ref={boxRef} style={{ width: '100px', height: '100px', backgroundColor: 'lightblue' }}>
         Resize Me
       </div>
