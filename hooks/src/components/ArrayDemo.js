44805cfe5028db946fcc30fea45f1c63bd849074
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-27 23:18:34 -0500

    Update ArrayDemo.js

--- hooks/src/components/ArrayDemo.js
+++ hooks/src/components/ArrayDemo.js
@@ -3,6 +3,16 @@ function ArrayDemo(){
   var color=["red","yellow","pink","green","blue","black"];
   return(
     <>
+    <div>
+    <ul>
+    {
+      color.map((x,ind)=>{
+       return <li>{x}{ind}</li>
+        
+      })
+    }
+    </ul>
+    </div>
     </>
   );
 }
