44c8e1793645b818bc8ec6e27b4af62ef5d323c6
Miss Trupti <truptiwagaskar4937@gmail.com>
2025-01-27 23:12:23 -0500

    Update FormDemo.js

--- hooks/src/components/FormDemo.js
+++ hooks/src/components/FormDemo.js
@@ -2,6 +2,27 @@
 function FormDemo(){
   return(
     <>
+     <h3>Uncontrolled Form!!</h3>
+        <form>
+        <div>
+            <div>
+            UserName:<input type="text" ref={txtUname}></input>
+            </div>
+            <div>
+                Password:<input type="text" ref={txtPassword}></input>
+
+            </div>
+            <div>
+                <input type="submit" value="Submit"  onClick={funSubmit}></input>
+            </div>
+            <div>
+            <input type="button" value="Read"  onClick={funRead}></input>
+            </div>
+            <div>
+            <input type="button" value="Remove"  onClick={funRemove}></input>
+            </div>
+        </div>
+        </form>
     </>
   );
 }
